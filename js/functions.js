0a5c6d3685d4f4d18ed1819feed5454232ee1c91
Deniel Pereira <deniel_16@live.com>
2020-09-07 19:05:00 -0300

    insere filmes, paginas e outros

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,35 @@
 $(document).ready(function(){
 
-  //Configurações do carousel
+  //Configurações do carousel dos filmes
+  $('.movies').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    focusOnSelect: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
+  //Configurações do carousel do footer
   $('.slick').slick({
     infinite: false,
     slidesToShow: 4,
@@ -11,4 +40,10 @@ $(document).ready(function(){
   $('.slick-prev').html("")
   $('.slick-next').html("")
 
+  $('.carousel').carousel({
+    interval: false,
+    ride: false
+  })
+
 });
+
