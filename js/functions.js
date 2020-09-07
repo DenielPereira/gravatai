8ce632db3becfe99cd69e58338ef631a2896e19a
Deniel Pereira <deniel_16@live.com>
2020-09-07 10:50:02 -0300

    estiliza botão desabilitado do carousel

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,14 @@
 $(document).ready(function(){
+
+  //Configurações do carousel
   $('.slick').slick({
     infinite: false,
     slidesToShow: 4,
     slidesToScroll: 1
   });
+
+  //Limpa o texto dos botões 'next' e 'previous'
   $('.slick-prev').html("")
   $('.slick-next').html("")
+
 });
